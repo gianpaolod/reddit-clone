d4551355c5ca5847f47938a7436ba3e6440123e0
Gianpaolo DAmato <damatohg@gmail.com>
2017-05-12 15:52:24 +0000

    working posts pages

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -37,4 +37,16 @@ describe PostsController, type: :controller do
      end
     end
   end
+  
+  describe 'GET #index' do
+    before(:each) do
+      4.times { FactoryGirl.create :post }
+      get :index
+    end
+    
+    it 'returns 4 records from de database' do
+      expect(response).to render_template :index
+    end
+  end
+  
 end
